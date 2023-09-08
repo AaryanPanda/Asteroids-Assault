656cf7e9e36d4eb55b09862d49b4d40ff1d80cf4
aaryan <aaryanpanda04@gmail.com>
2023-09-09 00:44:48 +0530

    worked on destroying asteroids and score updation

--- Game.js
+++ Game.js
@@ -1,11 +1,9 @@
-
-//display welcome popup
+// Display welcome popup
 window.addEventListener('load', function () {
     setTimeout(function () {
         var popup = document.getElementById('popup');
         popup.style.display = 'flex';
         popup.classList.toggle('fade-in');
-
 
         setTimeout(function () {
             popup.style.display = 'none';
@@ -13,101 +11,132 @@ window.addEventListener('load', function () {
     }, 0);
 });
 
-
-
-
-
-// Display the nickname in welcome popup
+// Display the nickname in the welcome popup
 const nickname = localStorage.getItem("nickname");
-
 document.getElementById("display-nick").textContent = nickname;
 
-
-//random numbers
-
+// Generate random numbers
 function generateRandomNums(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min
+    return Math.floor(Math.random() * (max - min )) + min;
 }
 
+// Define asteroid IDs
+let asteroidID = 0;
+let asteroid;
 
+// Generate asteroids
+function generateAsteroid() {
+    const imageNum = generateRandomNums(1, 6);
+    const classNum = generateRandomNums(1, 3);
 
-// move asteroids towards earth
-let gameBody = document.getElementById("game-body")
-let asteroid1ID = 0
-let asteroid1;
-let asteroid2ID = 1
-let asteroid2;
-let asteroid3ID = 2
-let asteroid3;
-let asteroid4ID = 3
-let asteroid4;
-
-
-//generate asteroids
-function generateAsteroids() {
-    let imageNum1 = generateRandomNums(1, 6)
-    let classNum = generateRandomNums(1, 3)
-
-    gameBody.innerHTML += `<img src=./Assets/Asteroids/asteroid${imageNum1}.png  class="asteroid-image${classNum} target" id=asteroid${asteroid1ID}>`
-    asteroid1 = document.getElementById(`asteroid${asteroid1ID}`)
-
-    let viewWidth1 = generateRandomNums(0, 100)
-    asteroid1.style.transform = `translateX(${viewWidth1}vw)`
-    let second1 = generateRandomNums(5, 14)
-    asteroid1.style.animationDuration = `${second1}s`
-
-    // let imageNum2=generateRandomNums(1,6)
-
-    // gameBody.innerHTML+=`<img src=./Assets/Asteroids/asteroid${imageNum2}.png  class=asteroid-image2  id=asteroid${asteroid2ID}>`
-    // asteroid2 = document.getElementById(`asteroid${asteroid2ID}`)
-
-    // let viewWidth2 = generateRandomNums(0,100)
-    // asteroid2.style.transform=`translateX(${viewWidth2}vw)`
-    // let second2 = generateRandomNums(3,7)
-    // asteroid2.style.animationDuration= `${second2}s`   
-
-    let imageNum3 = generateRandomNums(1, 6)
-
-    gameBody.innerHTML += `<img src=./Assets/Asteroids/asteroid${imageNum3}.png  class="asteroid-image3 target" id=asteroid${asteroid3ID}>`
-    asteroid3 = document.getElementById(`asteroid${asteroid3ID}`)
-
-    let viewHeight1 = generateRandomNums(-50, 50)
-    asteroid3.style.transform = `translateY(${viewHeight1}vh)`
-    let second3 = generateRandomNums(7, 9)
-    asteroid3.style.animationDuration = `${second3}s`
-
-
-
-
-
-    let imageNum4 = generateRandomNums(1, 6)
-
-    gameBody.innerHTML += `<img src=./Assets/Asteroids/asteroid${imageNum4}.png  class="asteroid-image4 target"  id=asteroid${asteroid4ID}>`
-    asteroid4 = document.getElementById(`asteroid${asteroid4ID}`)
-
-    let viewHeight2 = generateRandomNums(-50, 50)
-    asteroid4.style.transform = `translateY(${viewHeight2}vh)`
-    let second4 = generateRandomNums(4, 10)
-    asteroid4.style.animationDuration = `${second4}s`
-
-
-
-
-
+    asteroid = document.createElement("img");
+    asteroid.src = `./Assets/Asteroids/asteroid${imageNum}.png`;
+    asteroid.classList.add(`asteroid-image${classNum}`, "asteroid");
+    asteroid.dataset.id = asteroidID;
+    
+    const viewWidth = generateRandomNums(0, 100);
+    asteroid.style.transform = `translateX(${viewWidth}vw)`;
+    
+    const second = generateRandomNums(5, 14);
+    asteroid.style.animationDuration = `${second}s`;
+    
+    asteroid.addEventListener('click', destroyAsteroid);
+    
+    gameBody.appendChild(asteroid);
+    asteroidID++;
+        
 }
 
-generateAsteroids()
-
-//Call destroy asteroids function 
-gameBody.addEventListener('click', (e) => {
-    destroyAsteroids(e)
+// Add a single event listener to the gameBody to handle asteroid clicks
+const gameBody = document.getElementById("game-body");
+gameBody.addEventListener('click', (event) => {
+    event.stopPropagation();
 });
 
-function destroyAsteroids(e) {
+// Function to destroy asteroids
+function destroyAsteroid(event) {
+    const clickedAsteroid = event.target;
+    console.log(clickedAsteroid)
+    clickedAsteroid.style.display = 'none';
+    updateScore()
 
-    if (e.target.classList.contains("target")) {
-        e.target.style.display = 'none';
-        generateAsteroids()
-    }
+    // Remove the click event listener to prevent further clicks on this asteroid
+    clickedAsteroid.removeEventListener('click', destroyAsteroid);
 
+    // Generate a new asteroid to replace the destroyed one
+    generateAsteroid();
+}
+
+// Initial asteroid generation
+generateAsteroid();
+generateAsteroid();
+generateAsteroid();
+
+
+
+
+
+
+
+//updating score and lives function
+let score = 0;
+let lives = 3;
+
+// function updateScore() {
+//     score += 10; 
+//     document.getElementById("score").textContent = score;
+// }
+
+// function deductLife() {
+
+//     if (asteroid.getBoundingClientRect().top>190) {
+
+
+//     lives--;
+
+//         if (lives >= 0) {
+//         document.getElementById(`life${lives + 1}`).style.display = 'none'; 
+//         }
+        
+//         if (lives === 0) {
+//             window.location.href="index.html"
+//         }
+//     }
+
+
+
+// }
+
+//updating score and lives function
+
+
+function updateScore() {
+    score += 10; 
+    document.getElementById("score").textContent = score;
+}
+
+function deductLife() {
+    const asteroids = document.querySelectorAll('.asteroid');
+
+    asteroids.forEach((asteroid) => {
+        const asteroidRect = asteroid.getBoundingClientRect();
+
+        if (asteroidRect.top > 150) {
+            // Hide the asteroid
+            asteroid.style.display = 'none';
+
+            // Deduct a life
+            lives--;
+
+            if (lives >= 0) {
+                // Hide the corresponding life image
+                document.getElementById(`life${lives + 1}`).style.display = 'none'; 
+            }
+            
+            if (lives === 0) {
+                // Redirect to a different page when lives reach 0
+                window.location.href = "index.html";
+            }
+        }
+    });
 }
